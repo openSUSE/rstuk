de62c956e86146342ce03781a5d766ce5a5e4c37
Theo Chatzimichos <tchatzimichos@suse.com>
2013-04-12 17:31:14 +0200

    Add wait_for function that (what a surprise) waits for ssh connection
    It is heavily based on ssh_accessible? function of studio_spec

--- lib/rstuk.rb
+++ lib/rstuk.rb
@@ -1,14 +1,23 @@
 require 'open3'
+require 'socket'
+require 'timeout'
+
+class SSHFail < RuntimeError
+  def initialize server, port
+    super "Couldn't get connection to #{server}:#{port}"
+  end
+end
 
 class Shell
 
-  def self.remote server, port, cmd, expected_exit_status = 0
-    # We want to avoid host key interactive accept question
-    # And avoid test failures due to non English Locale
+  def self.remote server, port, cmd, opts = {}
+    defaults = {exit_code: 0, timeout: 90}
+    opts = defaults.merge opts
+    self.wait_for server, port, opts[:timeout]
     puts "ssh: #{cmd}"
     self.do \
       "ssh root@#{server}  #{self.ssh_options} -p #{port} \"#{cmd}\"",
-      expected_exit_status
+      opts[:exit_code]
   end
 
   def self.local cmd, expected_exit_status = 0
@@ -24,6 +33,7 @@ class Shell
 private
 
   def self.do cmd, expected_exit_status
+    # We want to avoid test failures due to non English Locale
     self.verify \
       expected_exit_status,
       *(Open3.capture3 "LANG=C; #{cmd}")
@@ -41,7 +51,26 @@ private
   end
 
   def self.ssh_options
+    # We want to avoid host key interactive accept question
     "-o 'UserKnownHostsFile /dev/null' -o StrictHostKeyChecking=no"
+  end
+
+  def self.wait_for server, port, seconds
+    puts "Waiting for ssh."
+    Timeout::timeout(seconds) do
+      tcp_banner = nil
+      until tcp_banner =~ /^SSH-2.0-OpenSSH.*/
+        begin
+          socket = TCPSocket.new server, port
+          tcp_banner = socket.readline.strip
+          socket.close
+        rescue Errno::ECONNREFUSED, Errno::EHOSTUNREACH, EOFError, Errno::ETIMEDOUT
+          # Go through the loop again
+        end
+      end
+    end
+  rescue Timeout::Error
+    raise SSHFail.new server, port
   end
 
 end
